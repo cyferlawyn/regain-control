d250fcd5fac5d3aa40745465d47ca6f7a64824d5
cyferlawny <cyferlawny@github.com>
2017-04-16 14:42:12 +0200

    First checkin from Spring Initializer

---
+++ src/main/java/de/cydev/RegainControlApplication.java
@@ -0,0 +1,12 @@
+package de.cydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegainControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegainControlApplication.class, args);
+	}
+}
